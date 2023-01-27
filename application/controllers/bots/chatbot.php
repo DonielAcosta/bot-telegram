06fe8aa833dc51f1fae3590b77a9a2f22bfdd960
doniel <donielacosta1995@gmail.com>
2023-01-27 08:53:26 -0400

    documentando codigo

--- application/controllers/bots/chatbot.php
+++ application/controllers/bots/chatbot.php
@@ -1,11 +1,23 @@
 <?php
 
 class Chatbot extends controller {
+    public $prueba=false;
 
     public function __construct() {
         parent::__construct();
     }
     
+   /**
+    * 1. Obtenga la actualización de la API de Telegram
+    * 2. Comprueba si la actualización está vacía
+    * 3. Decodificar la actualización
+    * 4. Guarde la actualización en un archivo de registro
+    * 5. Obtenga la identificación del chat
+    * 6. Recibe el mensaje
+    * 7. Llame a la función de estructura
+    * 8. Llame a la función inventariosedes
+    * 9. Llamar a la función direccionSedes
+    */
     public function index() {
         $getupdate = file_get_contents("php://input"); 
         if(empty($getupdate)){
@@ -20,18 +32,28 @@ class Chatbot extends controller {
         $this->struct($update);
         $this->inventariosedes($chatid,$message);
         $this->direccionSedes($chatid,$message);
-
     }
 
+    /**
+     * Envía un mensaje al chatid proporcionado.
+     * 
+     * @param chatid El ID de chat del usuario al que desea enviar el mensaje.
+     * @param response El mensaje que desea enviar.
+    */
     public function sendMessages($chatid,$response){
         $token  = $this->datasis->dameval('SELECT token FROM bots WHERE id = 13');
-
         $link   = 'https://api.telegram.org/bot'.$token;
         $url    = $link.'/sendMessage?chat_id='.$chatid.'&parse_mode=HTML&text='.urlencode($response); 
         $resp   = file_get_contents($url);
-        
     }
     
+    /**
+     * Envía una foto al usuario.
+     * 
+     * @param chatid El ID de chat del usuario al que desea enviar el mensaje.
+     * 
+     * @return La respuesta de la API de Telegram.
+    */
     public function img($chatid){
         $resp  = $this->datasis->damereg("SELECT * FROM bots ");
         $token = $resp['token'];
@@ -46,6 +68,12 @@ class Chatbot extends controller {
         return $resp;
     }
 
+    /**
+     * Una función que le permite enviar un archivo al usuario.
+     * 
+     * @param chatid El ID de chat del usuario que envió el mensaje.
+     * @param message El mensaje enviado por el usuario.
+    */
     public function inventariosedes($chatid,$message){ 
         setlocale(LC_ALL, "en_US.utf8");
         $message = iconv("utf-8", "ascii//TRANSLIT", $message);
@@ -71,6 +99,13 @@ class Chatbot extends controller {
         }
     }
 
+    /**
+     * Toma una cadena, la convierte a ascii, luego la compara con un campo de la base de datos y, si
+     * coincide, envía un mensaje al usuario.
+     * 
+     * @param chatid La identificación de chat del usuario al que desea enviar el mensaje.
+     * @param message El mensaje enviado por el usuario.
+     */
     public function direccionSedes($chatid,$message){
         setlocale(LC_ALL, "en_US.utf8");
         $message = iconv("utf-8", "ascii//TRANSLIT", $message);
@@ -92,6 +127,12 @@ class Chatbot extends controller {
         }
     }
 
+    /**
+     * Envía un archivo al usuario.
+     * 
+     * @param chatid El ID de chat del usuario al que desea enviar el mensaje.
+     * @param url La URL del archivo a enviar.
+     */
     public function files($chatid,$url){
         $resp  = $this->datasis->damereg("SELECT * FROM bots ");
         $token = $resp['token'];
@@ -116,9 +157,14 @@ class Chatbot extends controller {
         curl_close($ch);
     }
 
-	// Guarda el mensaje recibido de telegram
+    // Guarda el mensaje recibido de telegram
+    /**
+     * Toma los datos de la API de Telegram y los inserta en una base de datos
+     * 
+     * @param registro La matriz de datos que se envió al webhook.
+     */
     public function guardalog($registro){
-		$data = [];
+        $data = [];
         $data['message_id']     = $registro['message']['message_id'];
         $data['id_bot']         = $registro['message']['chat']['id'];
         $data['is_bot']         = $registro['message']['from']['is_bot'];
@@ -136,8 +182,15 @@ class Chatbot extends controller {
 
         memowrite($this->db->insert_string('logtelg',$data));
         $this->db->insert('logtelg',$data);
-	}
+    }
 
+    // Esto hace ?????
+    /**
+     * Toma el mensaje enviado por el usuario y verifica si coincide con alguno de los comandos en la base
+     * de datos. Si lo hace, envía la respuesta al usuario.
+     * 
+     * @param registro La matriz de datos que Telegram envía a su bot.
+    */
     public function struct($registro){
 
         $chatid         = $registro['message']['chat']['id'];
@@ -181,56 +234,57 @@ class Chatbot extends controller {
                 $this->img($chatid);
                 break;
             case 'hola':
-                $this->sendMessages($chatid,$this->datasis->dameval('SELECT consulta FROM telegram WHERE id = 18'));
+                $this->sendMessages($chatid,$this->datasis->dameval('SELECT consulta FROM telegram WHERE id = 19'));
                 break;
             case 'bien':
-                $this->sendMessages($chatid,$this->datasis->dameval('SELECT consulta FROM telegram WHERE id = 19'));
+                $this->sendMessages($chatid,$this->datasis->dameval('SELECT consulta FROM telegram WHERE id = 20'));
+                break;
+
             default:
-                $respuesta = $this->datasis->dameval('SELECT consulta FROM telegram WHERE id = 11'); 
-                $mSQL = str_replace('busqueda', $message, $respuesta); //para buscar la consulta en base de datos 
-                $query = $this->db->query(''.$mSQL.''); 
-                if($query->num_rows() > 0){
-                    $response = '';
-                    foreach( $query->result() as $row ){
-                        $response .= $row->codigo;
-                        $response .= ' '.$row->descrip;
-                        $response .= ' (Ex.'.nformat($row->existen,0).')';
-                        $response .= "\n";
-                    }
-                    $this ->sendMessages($chatid,$this->datasis->dameval('SELECT comando FROM telegram WHERE id = 11'));
-                    $this ->sendMessages($chatid,$response);
+            $respuesta = $this->datasis->dameval('SELECT consulta FROM telegram WHERE id = 11'); 
+            $mSQL = str_replace('busqueda', $message, $respuesta); //para buscar la consulta en base de datos 
+            $query = $this->db->query(''.$mSQL.''); 
+            if($query->num_rows() > 0){
+                $response = '';
+                foreach( $query->result() as $row ){
+                    $response .= $row->codigo;
+                    $response .= ' '.$row->descrip;
+                    $response .= ' (Ex.'.nformat($row->existen,0).')';
+                    $response .= "\n";
                 }
-                $centro = $this->datasis->dameval('SELECT consulta FROM telegram WHERE id = 12'); 
-                $mSQL = str_replace('busqueda', $message, $centro); //para buscar la consulta en base de datos 
-                $query = $this->db->query(''.$mSQL.''); 
-                if($query->num_rows() > 0){
-                    $response = '';
-                    foreach( $query->result() as $row ){
-                        $response .= $row->codigo;
-                        $response .= ' '.$row->descrip;
-                        $response .= ' (Ex.'.nformat($row->existen,0).')';
-                        $response .= "\n";
-                    }
-                    $this ->sendMessages($chatid, $this->datasis->dameval('SELECT comando FROM telegram WHERE id = 12'));
-                    $this ->sendMessages($chatid,$response);
+                $this ->sendMessages($chatid,$this->datasis->dameval('SELECT comando FROM telegram WHERE id = 11'));
+                $this ->sendMessages($chatid,$response);
+            }
+            $centro = $this->datasis->dameval('SELECT consulta FROM telegram WHERE id = 12'); 
+            $mSQL = str_replace('busqueda', $message, $centro); //para buscar la consulta en base de datos 
+            $query = $this->db->query(''.$mSQL.''); 
+            if($query->num_rows() > 0){
+                $response = '';
+                foreach( $query->result() as $row ){
+                    $response .= $row->codigo;
+                    $response .= ' '.$row->descrip;
+                    $response .= ' (Ex.'.nformat($row->existen,0).')';
+                    $response .= "\n";
                 }
-                $oriente = $this->datasis->dameval('SELECT consulta FROM telegram WHERE id = 13'); 
-                $mSQL = str_replace('busqueda', $message, $oriente); //para buscar la consulta en base de datos 
-                $query = $this->db->query(''.$mSQL.''); 
-                if($query->num_rows() > 0){
-                    $response = '';
-                    foreach( $query->result() as $row ){
-                        $response .= $row->codigo;
-                        $response .= ' '.$row->descrip;
-						$response .= ' (Ex.'.nformat($row->existen,0).')';
-                        $response .= "\n";
-                    }
-                    $this ->sendMessages($chatid,$this->datasis->dameval('SELECT comando FROM telegram WHERE id = 13'));
-                    $this ->sendMessages($chatid,$response);
+                $this ->sendMessages($chatid,$this->datasis->dameval('SELECT comando FROM telegram WHERE id = 12'));
+                $this ->sendMessages($chatid,$response);
+            }
+
+            $oriente = $this->datasis->dameval('SELECT consulta FROM telegram WHERE id = 13'); 
+            $mSQL = str_replace('busqueda', $message, $centro); //para buscar la consulta en base de datos 
+            $query = $this->db->query(''.$mSQL.''); 
+            if($query->num_rows() > 0){
+                $response = '';
+                foreach( $query->result() as $row ){
+                    $response .= $row->codigo;
+                    $response .= ' '.$row->descrip;
+                    $response .= ' (Ex.'.nformat($row->existen,0).')';
+                    $response .= "\n";
                 }
+                $this ->sendMessages($chatid,$this->datasis->dameval('SELECT comando FROM telegram WHERE id = 13'));
+                $this ->sendMessages($chatid,$response);
+            }
             break;
         }
     }
 }
-
-?>
